3e7a4aa5a75c12c1048c3443bfbe664abb583b94
Simon Wheatley <simonw@codeforthepeople.com>
2013-07-12 14:51:25 +0100

    Avoid error if `$parent` doesn't exist.

--- mag.php
+++ mag.php
@@ -301,8 +301,10 @@ class CFTP_Magnificent {
 			// @TODO: Remove the below code if unnecessary
 			// $permalink = home_url() . str_replace( '%issue%', $post->post_name, $this->issue_permalink_structure );
 		} elseif ( 'article' == $post->post_type ) {
-			$parent = get_post( $post->post_parent );
-			$permalink = home_url() . str_replace( array( '%issue%', '%article%' ), array( $parent->post_name, $post->post_name ), $this->article_permalink_structure );
+			if ( $parent = get_post( $post->post_parent ) )
+				$permalink = home_url( str_replace( array( '%issue%', '%article%' ), array( $parent->post_name, $post->post_name ), $this->article_permalink_structure ) );
+			else 
+				$permalink = '';
 		}
 		return $permalink;
 	}
