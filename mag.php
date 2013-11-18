ed19ea1a24dc088734baf2423c677e5c642d436e
Simon Wheatley <simonw@codeforthepeople.com>
2013-11-18 15:19:46 +0000

    Very important change

--- mag.php
+++ mag.php
@@ -11,6 +11,22 @@ Author URI: http://codeforthepeople.com/
  
 /*  Copyright 2013 Code for the People Ltd
 
+                _____________
+               /      ____   \
+         _____/       \   \   \
+        /\    \        \___\   \
+       /  \    \                \
+      /   /    /          _______\
+     /   /    /          \       /
+    /   /    /            \     /
+    \   \    \ _____    ___\   /
+     \   \    /\    \  /       \
+      \   \  /  \____\/    _____\
+       \   \/        /    /    / \
+        \           /____/    /___\
+         \                        /
+          \______________________/
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
